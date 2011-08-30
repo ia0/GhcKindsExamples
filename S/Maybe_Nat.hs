025ef3abd33f4c879ac2fa6d9fc3f500bb3f2fdd
Julien Cretin <github@ia0.eu>
2011-08-30 14:29:03 +0200

    Maybe_Nat

---
+++ S/Maybe_Nat.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE RankNTypes #-}
+
+module S.Maybe_Nat where
+
+import S.Nat( Nat(..) )
+import qualified S.Nat as N
+
+data Rep :: Maybe Nat -> * where
+  SNothing :: Rep Nothing
+  SJust :: N.Rep n -> Rep (Just n)
+
+fromRep :: Rep m -> Maybe Nat
+fromRep SNothing = Nothing
+fromRep (SJust n) = Just (N.fromRep n)
+
