0718f36673e5b93856462b4abd8964e062556b35
ia0 <git@ia0.eu>
2011-11-04 14:27:18 +0100

    01

--- tests/01-tidying.hs
+++ tests/01-tidying.hs
@@ -9,41 +9,6 @@ zonkQuantifiedTyVars.
 
 -}
 
-module M where
+module ShouldCompile where
 
 data T a b = K
-
-{-
-
-Current output:
-
-[1 of 1] Compiling M                ( 01-tidying.hs, 01-tidying.o )
-TYPE SIGNATURES
-TYPE CONSTRUCTORS
-  data T (k::BOX) (k::BOX) (a::k) (b::k)
-      RecFlag NonRecursive
-      = K :: forall (k::BOX) (k::BOX) (a::k) (b::k). T k k a b
-      FamilyInstance: none
-COERCION AXIOMS
-Dependent modules: []
-Dependent packages: [base, ghc-prim, integer-gmp]
-
-==================== Typechecker ====================
-
-
-Expected output:
-
-[1 of 1] Compiling M                ( 01-tidying.hs, 01-tidying.o )
-TYPE SIGNATURES
-TYPE CONSTRUCTORS
-  data T (k::BOX) (k0::BOX) (a::k) (b::k0)
-      RecFlag NonRecursive
-      = K :: forall (k::BOX) (k0::BOX) (a::k) (b::k0). T k k0 a b
-      FamilyInstance: none
-COERCION AXIOMS
-Dependent modules: []
-Dependent packages: [base, ghc-prim, integer-gmp]
-
-==================== Typechecker ====================
-
--}
